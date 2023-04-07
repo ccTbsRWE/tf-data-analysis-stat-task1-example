b2b520c685b6ee4b1723a5c74356106c90ff8a54
ccTbsRWE <130173273+ccTbsRWE@users.noreply.github.com>
2023-04-07 20:03:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,8 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 611202811 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    a = (x.mean() + 20)/10
+    return a # Ваш ответ
